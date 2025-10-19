af694845f1624802d890ca714617a9b4e8851105
Matilda <153890630+Bobateaaa@users.noreply.github.com>
2025-10-19 17:10:59 -0400

    Avancement | css du sous-menu mobile, animation sous-menu

--- menu.js
+++ menu.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const burger = document.querySelector(".menu-burger");
   const menu = document.querySelector(".menu-page");
   const body = document.body;
+  const MenuItems = document.querySelectorAll(".menu-page .menu li");
 
   if (!burger || !menu) return;
 
@@ -9,5 +10,12 @@ document.addEventListener("DOMContentLoaded", function () {
     burger.classList.toggle("open");
     menu.classList.toggle("open");
     body.classList.toggle("menu-open");
+  });
+
+    //Pour chaque menu(item) contenant un sous-menu
+    MenuItems.forEach(item => {
+      item.addEventListener('click', function(ev) {
+        item.classList.toggle('open');
+    });
   });
 });
