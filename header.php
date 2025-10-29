8c73c19b4c3ed180f18a21f1ce03839463930cfa
Matilda <153890630+Bobateaaa@users.noreply.github.com>
2025-10-28 23:22:06 -0400

    Avancement CSS footer et header adaptatif + ajouts commentaires

--- header.php
+++ header.php
@@ -4,36 +4,51 @@
  */
 ?>
 
-<div class="HeaderTopMargin">
+
+
+<div class="header-margin">
   <!-- Espace en haut de la page pour mobile -->
 </div>
 
-<header class="site-header">
-  <h1 class="logo"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+  <!-- Conteneur principal du header -->
+<span class="header-container">
+  <header class="site-header">
+    <h1 class="logo"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
 
-  <div class="menu-burger">
-    <span></span>
-    <span></span>
-    <span></span>
+    <!-- menu burger -->
+    <div class="menu-burger">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </header>
+
+  <!-- Menu principale -->
+  <div class="menu-page">
+    <div class="menu-content">
+
+      <!-- Barre de recherche -->
+        <form role="search" method="get" class="search-form menu-search-mobile" action="<?php echo home_url('/'); ?>">
+          <input type="search" class="search-field" placeholder="Recherche..." value="<?php echo get_search_query(); ?>" name="s">
+          <button type="submit">Rechercher</button>
+        </form>
+
+      <!-- Menu affichage dynamique -->
+      <?php
+        wp_nav_menu(array(
+          'theme_location' => 'main-menu',
+          'container' => false,
+          'menu_class' => 'menu',
+        ));
+      ?>
+    </div>
   </div>
-</header>
 
-<div class="menu-page">
-  <div class="menu-content">
-
-    <!-- Barre de recherche -->
+  <!-- Barre de recherche desktop -->
+  <div class="menu-search-desktop">
       <form role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
         <input type="search" class="search-field" placeholder="Recherche..." value="<?php echo get_search_query(); ?>" name="s">
-        <button type="submit">Rechercher</button>
+        <button type="submit"></button>
       </form>
-
-    <!-- Menu affichage dynamique -->
-    <?php
-      wp_nav_menu(array(
-        'theme_location' => 'main-menu',
-        'container' => false,
-        'menu_class' => 'menu',
-      ));
-    ?>
   </div>
-</div>
+</span>
