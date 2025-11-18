e6f0da477658facb6be70657e34746596c304fbf
Alexis David <alexis.david.111@gmail.com>
2025-11-18 14:19:44 -0500

    micro changements avant lien de contact contact.css dans function.php

--- contact.php
+++ contact.php
@@ -3,14 +3,19 @@
 Template Name: Contact
 */
 
-
 ?>
+
 <body>
     <?php get_header(); //appelle le header ?>
-    
+
     <div class="forumContact">
-        <span class="emailContact"></span>
-        <span class="objet"></span>
+        <h1>Contactez un des membres de TimVision</h1>
+        <input type="text" class="prenom" placeholder="Votre prénom">
+        <input type="text" class="nom" placeholder="Votre nom">
+        <input type="email" class="emailContact" placeholder="Qui voudriez-vous rejoindre?">
+        <input type="text" class="numeroMobile" placeholder="Votre mobile">
+        <textarea required></textarea>
+        <input type="submit" value="send" id="bouton">
         <span class="message"></span>
     </div>
     
