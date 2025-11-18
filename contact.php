1ea6a985aa92fd7fa71e054052d76f80bb1184f4
Alexis David <alexis.david.111@gmail.com>
2025-11-18 15:53:37 -0500

    progrès page contact.css, débuggage function fixed

--- contact.php
+++ contact.php
@@ -8,11 +8,15 @@ Template Name: Contact
     <?php get_header(); //appelle le header ?>
 
     <div class="forumContact">
-        <h1>Contactez un des membres de TimVision</h1>
+        <div class="messagePageContact">
+            <h1>Contactez un des membres de</h1>
+            <img class="footer-logo-img" src="<?php echo get_template_directory_uri(); ?>/Images/Texte.png" alt="Logo de l'expo TIM">
+        </div>
+        
         <input type="text" class="prenom" placeholder="Votre prénom">
         <input type="text" class="nom" placeholder="Votre nom">
-        <input type="email" class="emailContact" placeholder="Qui voudriez-vous rejoindre?">
         <input type="text" class="numeroMobile" placeholder="Votre mobile">
+        <input type="email" class="emailContact" placeholder="Qui voudriez-vous rejoindre?">
         <textarea required></textarea>
         <input type="submit" value="send" id="bouton">
         <span class="message"></span>
