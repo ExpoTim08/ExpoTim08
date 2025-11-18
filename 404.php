51a68417066f6034e5c59968698515c11e63637e
RemyRoger <e2364643@cmaisonneuve.qc.ca>
2025-11-18 14:18:13 -0500

    page 404

---
+++ 404.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+
+Template Name: Page 404
+
+*/ 
+?>
+<?php get_header(); ?>
+
+<body>
+    <main>
+        <section>
+            <h1>Erreur 404</h1>
+            <p>La page que vous cherchez n'existe pas</p>
+            <button></button>
+        </section>
+    </main>
+</body>
+
+<?php get_footer(); ?>
