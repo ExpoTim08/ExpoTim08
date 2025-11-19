b018709b4fa01a151b712a2509974cd5f1811d7d
RemyRoger <e2364643@cmaisonneuve.qc.ca>
2025-11-18 20:22:49 -0500

    404 suite et linear gradient

--- 404.php
+++ 404.php
@@ -12,12 +12,17 @@ require("global.php");
             <div class="border droite"></div>
             <section class="section-erreur">
                  <h1 class="titre-erreur" aria-label="erreur">
-                    <span class="titre-erreur-layer titre-erreur--base">ERREUR 404</span>
-                    <span class="titre-erreur-layer titre-erreur--layer--1">ERREUR 404</span>
-                    <span class="titre-erreur-layer titre-erreur--layer--2">ERREUR 404</span>
-                    <span class="titre-erreur-layer titre-erreur--layer--3">ERREUR 404</span>
-                    <p class="message">La page que vous cherchez n'existe pas</p>
+                    <span class="titre-erreur-layer titre-erreur-base">ERREUR 404</span>
+                    <span class="titre-erreur-layer titre-erreur-layer--1">ERREUR 404</span>
+                    <span class="titre-erreur-layer titre-erreur-layer--2">ERREUR 404</span>
+                    <span class="titre-erreur-layer titre-erreur-layer--3">ERREUR 404</span>
                  </h1>
+                  <p class="message">La page que vous cherchez n'existe pas</p>
+            </section>
+            <section>
+                <h2>Projet 1</h2>
+                <h2>Projet 2</h2>
+                <h2>Projet 3</h2>
             </section>
         </main>
     </div>
