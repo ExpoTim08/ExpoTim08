452eeaa2cf103258c883ff0b591aeaf40399a0d4
Alexis David <alexis.david.111@gmail.com>
2025-11-20 16:20:44 -0500

    Page contact termine, adaptable en mobile, incorporation section titre en cours

--- contact.php
+++ contact.php
@@ -8,18 +8,20 @@ Template Name: Contact
     <?php get_header(); //appelle le header ?>
 
     <div class="forumContact">
-        <div class="messagePageContact">
-            <h1>Contactez un des membres de</h1>
-            <img class="footer-logo-img" src="<?php echo get_template_directory_uri(); ?>/Images/Texte.png" alt="Logo de l'expo TIM">
-        </div>
+        <form>
+            <div class="messagePageContact">
+                <h1>Contactez un des membres de</h1>
+                <img class="footer-logo-img" src="<?php echo get_template_directory_uri(); ?>/Images/Texte.png" alt="Logo de l'expo TIM">
+            </div>
         
-        <input type="text" class="prenom" placeholder="Votre prénom">
-        <input type="text" class="nom" placeholder="Votre nom">
-        <input type="text" class="numeroMobile" placeholder="Votre mobile">
-        <input type="email" class="emailContact" placeholder="Qui voudriez-vous rejoindre?">
-        <textarea required></textarea>
-        <input type="submit" value="send" id="bouton">
-        <span class="message"></span>
+            <input type="text" class="prenom" placeholder="Votre prénom">
+            <input type="text" class="nom" placeholder="Votre nom">
+            <input type="text" class="numeroMobile" placeholder="Votre mobile">
+            <input type="email" class="emailContact" placeholder="Qui voudriez-vous rejoindre?">
+            <h4>Votre message.</h4>
+            <textarea required></textarea>
+            <input type="submit" value="Envoyer" id="bouton">
+        </form>
     </div>
     
     <?php get_footer(); //appelle le footer ?>
