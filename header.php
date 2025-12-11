f049cc1af0fd3510a5484273066c10f7ac56285c
Matilda <153890630+Bobateaaa@users.noreply.github.com>
2025-12-11 12:06:36 -0500

    accueil ajustement

--- header.php
+++ header.php
@@ -16,6 +16,7 @@ $logo = get_theme_mod('expoTim_logo');
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="format-detection" content="telephone=no">
   <?php wp_head(); ?>
 </head>
 
